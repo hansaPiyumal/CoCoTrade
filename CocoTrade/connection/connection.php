2fe1c05ed5fb77ae75bfceac8b596d38ec1f074d
hansaPiyumal <hansapiyumal9@gmail.com>
2025-05-21 17:20:26 +0530

    open market for coconuts and coconuts rekated products

--- CocoTrade/connection/connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $servername = "127.0.0.1";
-    $username = "root";
-    $password = "";
-    $database = "cocotrade";
-
-    $connection = mysqli_connect($servername,$username,$password,$database);
-?>
