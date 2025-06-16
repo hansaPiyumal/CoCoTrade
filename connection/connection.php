445f17c105f6e036404aef2b8fc5e61457aef253
AloneSweety <139615872+AloneSweety@users.noreply.github.com>
2025-06-16 18:56:38 +0000

    edit

--- connection/connection.php
+++ connection/connection.php
@@ -1,10 +1,15 @@
 <?php
-$servername = "127.0.0.1";
-$username = "root";
-$password = "";
-$database = "cocotrade";
+$servername = '127.0.0.1';
+$username = 'root';
+$password = '';
+$database = 'cocotrade';
 
-$connection = mysqli_connect($servername, $username, $password, $database);
+$connection = mysqli_connect($servername,$username,$password,$database);
 
 
+if (!$connection) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+//echo "Connected successfully";
 ?>
